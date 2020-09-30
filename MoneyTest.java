afb2343fae87159b989783fe383c3bc3e6a2f17a
arthursl12 <arthursoutolima@gmail.com>
2020-09-30 12:25:46 -0300

    Money abstrata + fábricas

--- MoneyTest.java
+++ MoneyTest.java
@@ -12,5 +12,20 @@ public class MoneyTest{
         assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
+
+    @Test
+    public void testMultiplication() {
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
+    }
+    
+    @Test
+    public void testFrancMultiplication() {
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
+     }
 }
