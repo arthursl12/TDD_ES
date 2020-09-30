0cfb027e8a04c3405243b681e7d5ca5265d203e7
arthursl12 <arthursoutolima@gmail.com>
2020-09-30 13:01:42 -0300

    Finalização da junção de Franc e Dollar em Money

--- Main.java
+++ Main.java
@@ -3,7 +3,5 @@ import org.junit.runner.JUnitCore;
 class Main {
 	public static void main(String[] args) {
 		JUnitCore.main("MoneyTest");
-		JUnitCore.main("DollarTest");
-		JUnitCore.main("FrancTest");
 	}
 }
--- MoneyTest.java
+++ MoneyTest.java
@@ -8,25 +8,16 @@ import static org.junit.Assert.assertEquals;
 public class MoneyTest{
 	@Test
     public void testEquality() {
-        assertTrue(new Dollar(5, "USD").equals(new Dollar(5, "USD")));
-        assertFalse(new Dollar(5, "USD").equals(new Dollar(6, "USD")));
-        assertTrue(new Franc(5, "CHF").equals(new Franc(5, "CHF")));
-        assertFalse(new Franc(5, "CHF").equals(new Franc(6, "CHF")));
-        assertFalse(new Franc(5, "CHF").equals(new Dollar(5, "USD")));
-    }
+        assertTrue(Money.dollar(5).equals(Money.dollar(5))); 
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }	
 
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
-    }
-
-    @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
